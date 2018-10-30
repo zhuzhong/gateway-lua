f4e8c0d37061b78549207ed2ae6f8016179f40d0
zhuzhong <zhuzhong@AladingBank.com>
2018-10-30 09:21:42 +0800

    增加路由及负载均衡方法

--- lua/set_by.lua
+++ lua/set_by.lua
@@ -1,10 +1,39 @@
+local math=require "math"
+local function s2table(servers)
+	table ts={}
+	--如果servers 不是多个，而是一个 split的行为是什么呢？测试一下
+	local server_arrays=string.split(servers,',')
+	for i=1;#server_arrays do
+		local s=server_arrays[i]
+		ts[i]=s
+	end	
+
+	table.sort(ts,function ( a,b )
+		-- body
+					if a<b then 	
+						return true;
+					else a>=b then 
+							return false;
+					end
+			end)
+	return ts
+end
+
+local function rdom_load_balance_server( ts )
+	-- body
+	local t_lenth=table.getn(ts);
+	math.randomseed(t_lenth)
+	local r=math.random(t_lenth)
+	return ts[r]
+end
+
+--------------------------------------------------------------------------------
 --[[
 根据请求的url地址 比如
 http://localhost/context_path/user/queryUserInfo/1
 从中截取context_path部分，然后路由至相应的后端服务器
 
 ]]
---local gw="gw"
 -- 请求的url地址
 local request_uri=ngx.var.request_uri
 ngx.log(ngx.ERR,"request_uri=",request_uri)
@@ -27,11 +56,19 @@ end
 
 
 local shared_data=ngx.shared.shared_data
-local server=shared_data:get(context_path)
-ngx.log(ngx.ERR,"server=",server)
-if server ==nil then 
-	server="127.0.0.1:8088"
+local servers=shared_data:get(context_path)
+ngx.log(ngx.ERR,"server=",servers)
+
+
+local ts=s2table(servers)
+--然后实现随机负载均衡算法,选择其中一个服务端
+local r_server=rdom_load_balance_server(ts)
+return 
+
+
+if r_server ==nil then 
+	r_server="127.0.0.1:8088"
 end 
 
-return "http://"..server
+return "http://"..r_server
 
