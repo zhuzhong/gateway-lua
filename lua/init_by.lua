03d307d91d0577af725f65ff537cdce0b91e2b39
zhuzhong <zhuzhong@AladingBank.com>
2018-10-30 14:29:41 +0800

    重构

--- lua/init_by.lua
+++ lua/init_by.lua
@@ -3,4 +3,5 @@ local shared_data=ngx.shared.shared_data
 
 shared_data:set("examples","127.0.0.1:8088")
 shared_data:set("docs","127.0.0.1:8089")
+shared_data:set("local","127.0.0.1:10000")
 
--- lua/set_by.lua
+++ lua/set_by.lua
@@ -1,4 +1,5 @@
 local math=require "math"
+
 local function s2table(servers)
 	table ts={}
 	--如果servers 不是多个，而是一个 split的行为是什么呢？测试一下
@@ -27,6 +28,23 @@ local function rdom_load_balance_server( ts )
 	return ts[r]
 end
 
+
+function get_server( context_path )
+	local shared_data=ngx.shared.shared_data
+	local servers=shared_data:get(context_path)
+	ngx.log(ngx.ERR,"server=",servers)
+	local ts=s2table(servers)
+--然后实现随机负载均衡算法,选择其中一个服务端
+	local r_server=rdom_load_balance_server(ts)
+	if r_server ==nil then 
+		--[[
+		对于动态服务找不相应的后端服务器，全部指定到一台静态服务器上,这个根据业务具体决定,
+		如何处理
+		]]
+		r_server="127.0.0.1:10000"
+	end 
+		return 'http://'..r_server
+end
 --------------------------------------------------------------------------------
 --[[
 根据请求的url地址 比如
@@ -40,8 +58,16 @@ ngx.log(ngx.ERR,"request_uri=",request_uri)
 --local uri=ngx.var.uri
 --ngx.log(ngx.ERR,"uri=",uri)
 local i,e=string.find(request_uri,"/")
+if e == nil then
+	return get_server('local')
+end
 --ngx.log(ngx.ERR,"i=",i," e=" ,e)
 local sub_request_uri=string.sub(request_uri,i+1)
+
+if string.len(sub_request_uri) == 0 then
+	return get_server('local')
+end
+
 ngx.log(ngx.ERR,"sub_request_uri=",sub_request_uri)
 
 local _,d=string.find(sub_request_uri,"/")
@@ -54,21 +80,6 @@ else
 	ngx.log(ngx.ERR,"c2=",context_path)
 end
 
+return get_server(context_path)
 
-local shared_data=ngx.shared.shared_data
-local servers=shared_data:get(context_path)
-ngx.log(ngx.ERR,"server=",servers)
-
-
-local ts=s2table(servers)
---然后实现随机负载均衡算法,选择其中一个服务端
-local r_server=rdom_load_balance_server(ts)
-return 
-
-
-if r_server ==nil then 
-	r_server="127.0.0.1:8088"
-end 
-
-return "http://"..r_server
 
